73cec14756c74c463357ad6f46d56e863a6f01ad
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-09 13:39:01 -0500

    :construction: work

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { ProductsController } from './controllers/products.controller';
+import { BrandsController } from './controllers/brands.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
+import { BrandsService } from './services/brands.service';
+import { CategoriesService } from './services/categories.service';
 
 @Module({
-  controllers: [ProductsController, CategoriesController],
-  providers: [ProductsService],
+  controllers: [ProductsController, CategoriesController, BrandsController],
+  providers: [ProductsService, BrandsService, CategoriesService],
 })
 export class ProductsModule {}
--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { Product } from './../entities/product.entity';
-import { CreateProductDto, UpdateProductDto } from './../dtos/products.dto';
+import { CreateProductDto, UpdateProductDto } from './../dtos/products.dtos';
 
 @Injectable()
 export class ProductsService {
