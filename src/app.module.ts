8198af8f3e784f2dbca75ba674c7e318d7599c84
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-10 15:39:15 -0500

    :construction: work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,13 +8,23 @@ const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,12 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private apiKey: string) {}
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[],
+  ) {}
   getHello(): string {
+    console.log(this.tasks);
     return `Hello World! ${this.apiKey}`;
   }
 }
