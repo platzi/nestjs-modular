6722dada93289b893eff4c8329a07397ad0df514
Nicolas Molina Monroy <hi@nicobytes.com>
2021-04-25 08:40:01 -0400

    :wrench: config

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3000);
+
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
