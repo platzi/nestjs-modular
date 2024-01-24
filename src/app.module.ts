f609616f2851462e464380d327d5058e1010afd5
Jaime <jaimevijuescafraca@gmail.com>
2024-01-24 12:02:12 +0100

    Add HttpService and TASKS provider to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { HttpService, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+
 
 const API_KEY = '123456';
 const API_KEY_PROD = '456789';
@@ -14,7 +15,15 @@ const API_KEY_PROD = '456789';
   {
     provide: 'API_KEY',
     useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-  }
+  },
+  {
+    provide: 'TASKS',
+    useFactory: async (http: HttpService) => {
+        const tasks = await http.get('https://jsonplaceholder.typicode.com/todos').toPromise();
+        return tasks.data;
+    },
+    inject: [HttpService],
+  },
 ],
 })
 
